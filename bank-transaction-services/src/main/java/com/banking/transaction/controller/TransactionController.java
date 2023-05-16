1acd429535692a46f0b0115150b6c49adb3b19be
Subhas C <bhagawat.subhasc@dxc.com>
2023-05-16 14:18:48 +0530

    test commit

--- bank-transaction-services/src/main/java/com/banking/transaction/controller/TransactionController.java
+++ bank-transaction-services/src/main/java/com/banking/transaction/controller/TransactionController.java
@@ -1,6 +1,7 @@
 package com.banking.transaction.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,12 @@ public class TransactionController {
 	@GetMapping("/test")
 	public String testMethod() {
 		return "Transaction test controller";
+	}
+
+	//test for github access
+	@PostMapping("/success")
+	public String postSuccess(){
+		return "Success";
 	}
 
 }
