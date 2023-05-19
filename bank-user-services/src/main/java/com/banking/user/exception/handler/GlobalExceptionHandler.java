c6c324881304cf413ccb117175fe791dfc56a39c
Arun Kumar Venkatesan <97891813+arunjavadxc@users.noreply.github.com>
2023-05-19 19:00:36 +0530

    Fixed mapping between user and user roles

--- bank-user-services/src/main/java/com/banking/user/exception/handler/GlobalExceptionHandler.java
+++ bank-user-services/src/main/java/com/banking/user/exception/handler/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.banking.user.exception.NotFoundException;
+import com.banking.user.exception.UserAlreadyExistException;
 import com.banking.user.utils.Response;
 
 @ControllerAdvice
@@ -21,8 +22,16 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(SQLException.class)
 	public ResponseEntity<Response<String>> sqlException(SQLException sqe) {
-		Response<String> response = new Response<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), sqe.getLocalizedMessage(), null);
+		Response<String> response = new Response<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), sqe.getLocalizedMessage(),
+				null);
 		return new ResponseEntity<>(response, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(UserAlreadyExistException.class)
+	public ResponseEntity<Response<String>> userAlreadyExistException(UserAlreadyExistException uae) {
+		Response<String> response = new Response<>(HttpStatus.NOT_ACCEPTABLE.value(), uae.getLocalizedMessage(),
+				null);
+		return new ResponseEntity<>(response, org.springframework.http.HttpStatus.NOT_ACCEPTABLE);
 	}
 
 }
